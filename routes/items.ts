fe7deb63be3f1f0b118d569bf6c70ff0747759ec
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-04-09 19:14:03 -0400

    Endpoints implementados

--- routes/items.ts
+++ routes/items.ts
@@ -13,7 +13,6 @@ Router.get('/', async (req: Request, res: Response) => {
 
     const uri = `https://api.mercadolibre.com/sites/MLA/search?q=${q}`
     const url: string = encodeURI(uri)
-    console.log({ id: req.params.id, uri, url })
 
     request.get(url, (error, response, body) => {
       if (error) res.status(500).send(error.message)
@@ -31,9 +30,27 @@ Router.get('/', async (req: Request, res: Response) => {
 // @access  Public
 Router.get('/:id', async (req: Request, res: Response) => {
   try {
-    const uri = `https://api.mercadolibre.com/items/​${req.params.id}`
+    const uri = `https://api.mercadolibre.com/items/${req.params.id}`
     const url: string = encodeURI(uri)
-    console.log({ id: req.params.id, uri, url })
+
+    request.get(url, (error, response, body) => {
+      if (error) res.status(500).send(error.message)
+
+      const result = JSON.parse(body)
+      res.status(200).send(result)
+    })
+  } catch (err) {
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/items/:id/description
+// @desc    Get item description by ID
+// @access  Public
+Router.get('/:id/description', async (req: Request, res: Response) => {
+  try {
+    const uri = `https://api.mercadolibre.com/items/${req.params.id}/description`
+    const url: string = encodeURI(uri)
 
     request.get(url, (error, response, body) => {
       if (error) res.status(500).send(error.message)

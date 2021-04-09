825815c845b2e2eb4043915f3ba5fe769d00824a
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-04-09 07:41:17 -0400

    Config for heroku

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
-const port = 3001
+const PORT = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Listening port ${port}`))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+  )
+}
+
+app.listen(PORT, () => console.log(`Listening port ${PORT}`))

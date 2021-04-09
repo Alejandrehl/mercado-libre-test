f9aee2860fdb289c6c5b1c76f3959c7f1e9870a9
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-04-09 19:07:22 -0400

    save

--- routes/items.ts
+++ routes/items.ts
@@ -11,14 +11,34 @@ Router.get('/', async (req: Request, res: Response) => {
     const { query } = req
     const { q } = query
 
-    const url: string = encodeURI(
-      `https://api.mercadolibre.com/sites/MLA/search?q=${q}`,
-    )
+    const uri = `https://api.mercadolibre.com/sites/MLA/search?q=${q}`
+    const url: string = encodeURI(uri)
+    console.log({ id: req.params.id, uri, url })
 
     request.get(url, (error, response, body) => {
       if (error) res.status(500).send(error.message)
 
       const result = JSON.parse(body).results
+      res.status(200).send(result)
+    })
+  } catch (err) {
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/items/:id
+// @desc    Get item by ID
+// @access  Public
+Router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const uri = `https://api.mercadolibre.com/items/​${req.params.id}`
+    const url: string = encodeURI(uri)
+    console.log({ id: req.params.id, uri, url })
+
+    request.get(url, (error, response, body) => {
+      if (error) res.status(500).send(error.message)
+
+      const result = JSON.parse(body)
       res.status(200).send(result)
     })
   } catch (err) {

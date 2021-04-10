d62df94ae2fadb2481852c9797a2a492fb31b56d
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-04-09 21:31:12 -0400

    save

--- client/src/pages/ItemDetail/index.tsx
+++ client/src/pages/ItemDetail/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router'
+import ItemContext from '../../context/Item/item.context'
 
 const ItemDetail: React.FC = () => {
+  const { loading, item, error } = useContext(ItemContext)
+  const { id }: any = useParams()
+  console.log({ id, loading, item, error })
+
   return (
     <div>
       <h1>ItemDetail</h1>

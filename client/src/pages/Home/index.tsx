42cfc6f3c9a2fac2627e01493d845238fd5739d6
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-04-10 10:19:16 -0400

    bootstrap agregado al proyecto

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   )
 }

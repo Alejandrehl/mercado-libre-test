1617b79ca16e11c07e5f34664f62f862237a188d
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-04-09 20:31:00 -0400

    pages

---
+++ client/src/pages/ItemDetail/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemDetail: React.FC = () => {
+  return (
+    <div>
+      <h1>ItemDetail</h1>
+    </div>
+  )
+}
+
+export default ItemDetail
---
+++ client/src/pages/ItemsSearch/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemsSearch: React.FC = () => {
+  return (
+    <div>
+      <h1>ItemsSearch</h1>
+    </div>
+  )
+}
+
+export default ItemsSearch
